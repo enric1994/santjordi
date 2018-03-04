a86bb1d2b013a17b02d18ebd52f24e5bb4258237
Enric <enric.moreu.filella@gmail.com>
2018-03-04 14:24:38 +0100

    subfolders organization

--- handler.py
+++ handler.py
@@ -2,19 +2,17 @@
 import random_number
 import japo
 
-#TODO Organize functions in subfolders
-
 def handle(chat,sender,message):
     #Random number
     if message[:8]=="##random":
         return random_number.generate(message[8:])
     
     #JapoBot
-    if message[:6]=="##japo":
-        return japo.organize(chat,sender,message[6:])
+    # if message[:6]=="##japo":
+    #     return japo.organize(chat,sender,message[6:])
 
-    else:
-        return -1
+    # else:
+    #     return -1
 
 #34669214506-1520096942@g.us
 #34669214506@c.us
