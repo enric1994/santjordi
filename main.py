5241c1a1b54a4bbc968f16da695962f3c614dd30
Enric Moreu <enric.moreu.filella@gmail.com>
2018-03-11 19:18:46 +0100

    python3 message handling

--- main.py
+++ main.py
@@ -15,17 +15,19 @@ offline_mode=False
 #Do the tests!!! $python -m unittest discover
 
 #Initialize bot
-if not offline_mode: driver = WhatsAPIDriver()
+if not offline_mode: driver = WhatsAPIDriver(username="enric")
 print("Waiting for QR")
 if not offline_mode: driver.wait_for_login()
+
+
 print("Bot started")
 
 #Handle messages
 def check_unread():
     if offline_mode:
-        chat=raw_input("Input chat: ")
-        sender=raw_input("Input sender: ")
-        input_message=raw_input("Input message: ")
+        chat=input("Input chat: ")
+        sender=input("Input sender: ")
+        input_message=input("Input message: ")
 
         print(handler.handle(chat,sender,input_message))
     else:
@@ -33,12 +35,13 @@ def check_unread():
         for contact in driver.get_unread():
             for message in reversed(contact.messages):
                 if isinstance(message, Message):
-                    response=handler.handle(contact.chat.id,message.sender.id,message.safe_content)
+                    print(message)
+                    response=handler.handle(contact.chat.id,message.sender.id,message.safe_content[:-3])
                     if response!=-1:
-                        time.sleep(random.randint(3,5))
+                        if not offline_mode: time.sleep(random.randint(3,5))
                         contact.chat.send_message(str(response))
                     else:
-                        print "No answer"
+                        print("No answer")
 
 #####Cron functions
 
@@ -55,7 +58,7 @@ schedule.every().day.at("14:07").do(les_planes_cron)
 #TODO Run message handling and cron in parallel
 while True:
     check_unread()
-    time.sleep(random.randint(5,10))
+    if not offline_mode: time.sleep(random.randint(5,10))
     schedule.run_pending()
 
 
