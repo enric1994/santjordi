003b1712807d05af87762dd973107f1b7bb71e5a
Enric Moreu <enric.moreu.filella@gmail.com>
2018-03-13 05:25:31 +0000

    bold removed, it is not diplayed in mobile app

--- apps/les_planes.py
+++ apps/les_planes.py
@@ -13,7 +13,7 @@ def message():
     tasks=("Avui al jove Marc li toca fer la cuina. Eric vas baixar les bosses ahir?","Avui la cuina li toca a l'Uri","Avui la cuina la farà l'Enric","Avui al David li toca fer la cuina","Avui l'Eric hauria de treure la rentaplats i posar tot el que estigui fora","Avui estaria bé rifar qui posa la rentaplats","Avui el jove Eric te la missió de baixar 3 bosses de basura")
     today_task=tasks[week_day]
 
-    string= """🐔*Bon dia nais*, es presenta una nou dia a la republica de les Planes
+    string= """🐔Bon dia nais, es presenta una nou dia a la republica de les Planes
     💰El Bitcoin esta a """+btc+"""$
     ⛅️Estem a una temperatura de: """+str(temp)[:5]+"\n"
     string=string+"""
