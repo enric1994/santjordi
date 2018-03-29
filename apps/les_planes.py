cdece32e995076b1745ac9ca8bbcffd522aa7200
Enric Moreu <enric.moreu.filella@gmail.com>
2018-03-29 14:11:56 +0200

    schedule changes tuesday-thursday

--- apps/les_planes.py
+++ apps/les_planes.py
@@ -10,9 +10,9 @@ def message():
     request_temp=requests.get('http://api.openweathermap.org/data/2.5/weather?lat=41.43175&lon=2.0935200000000123&APPID=926a70c3b3dc09c4a73944f38229ce74')
     temp=request_temp.json().get("main").get("temp")-273.15
     week_day=datetime.datetime.today().weekday()
-    tasks=("Avui al jove Marc li toca fer la cuina. Eric vas baixar les bosses ahir?","Avui la cuina li toca a l'Uri","Avui la cuina la farà l'Enric","Avui al David li toca fer la cuina","Avui l'Eric hauria de treure la rentaplats i posar tot el que estigui fora","Avui estaria bé rifar qui posa la rentaplats","Avui el jove Eric te la missió de baixar 3 bosses de basura")
+    tasks=("Avui al jove Marc li toca fer la cuina. Eric vas baixar les bosses ahir?","Avui al David li toca fer la cuina","Avui la cuina la farà l'Enric","Avui la cuina li toca a l'Uri","Avui l'Eric hauria de treure la rentaplats i posar tot el que estigui fora","Avui estaria bé rifar qui posa la rentaplats","Avui el jove Eric te la missió de baixar 3 bosses de basura")
     today_task=tasks[week_day]
-
+    
     string= """🐔Bon dia nais, es presenta una nou dia a la republica de les Planes
     💰El Bitcoin esta a *"""+btc+"""*$
     ⛅️Estem a una temperatura de: *"""+str(temp)[:5]+"*\n"
