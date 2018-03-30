fa40ae89d8cf2bf1f773b34cc2774bf9b64192b3
Enric Moreu <enric.moreu.filella@gmail.com>
2018-03-30 23:57:11 +0300

    first texts

--- apps/santjordi_texts.py
+++ apps/santjordi_texts.py
@@ -1,15 +1,102 @@
-welcome_princep="t'ha tocat ser el princep"
-welcome_princesa="t'ha tocat ser la princesa"
-welcome_rei="t'ha tocat ser el rei"
-welcome_pages="t'ha tocat ser el pages"
-welcome_drac="t'ha tocat ser el drac"
-welcome_bou="t'ha tocat ser el bou"
+welcome='''Hola! Estàs preparat per començar la *Llegenda de Sant Jordi* 🌹 ?
 
+Això és un *conte interactiu* en que viuràs la llegenda des del punt de vista d’un dels personatges. Per poder avançar en la història hauràs de trobar a determinats personatges.
+Per cert… No et preocupis per mi, jo només sóc un robot 🤖, que t’anirà narrant la història. 
+
+_*Fa molt i molt de temps* el poble de Montblanc era devastat per un monstre ferotge i terrible, que podia caminar, volar i nedar, i tenia un alè tant pudent, que des de molt lluny amb les seves alenades enverinava l'aire i produïa la mort a tots els qui el respiraven._
+
+'''
+
+welcome_cavaller='''En aquest conte t'ha tocat ser *el cavaller* 🤴🏻! .El teu nom és Jordi i et dediques a rondar pels pobles fent justícia, llueixes una brillant armadura i sempre vas sobre el teu cavall blanc.
+
+T’ha arribat la notícia de que un pagès té un problema terrible amb un ferotge animal que amenaça el seu ramat. Troba el pagès  👨‍🌾 per poder continuar amb la història
+'''
+
+cavaller_1='''Molt bé! Has trobat al pagès, el pobre està tant aterrat que no pot ni parlar. Després de 3 gerres de cervesa sembla que ja ha tornat en si . 
+
+Resulta que hi ha un drac ferotge que amenaça als ramats i a les persones, i per això s’ha decidit que cada dia es sortejarà quina persona s’entrega per tal de calmar al drac.
+
+El que més greu li sap al pagès és que el sorteig d’avui a senyalat a l’estimada princesa.
+
+Tu, l’intrèpid cavaller, et puges al cavall i sense pensar-ho dos cops et dirigeixes a socorre a la princesa. Troba a la princesa 👸🏼 per continuar la història.
+'''
+
+caballer_2='''caballer2'''
+
+welcome_princesa='''En aquest conte t'ha tocat ser la  princesa 👸🏼!  .Tens el cor robat a tots els ciutadans, per la teva bellesa i elegància.
+
+ Avui el rei t’ha convocat perquè t’ha de donar una notícia. Troba a al rei 🤴 per poder continuar la història
+ '''
+
+princesa_1='''Molt bé! Has trobat al rei, el qual sembla bastant disgustat. 
+
+Ell comença explican-te l’última llei que ha aprovat: cada dia s’entregarà una persona al drac, per tal de calmar la seva sed de sang i que pari de matar ramats sencers. La manera d’escollir la persona serà fent un sorteig on entraran tots els habitants del poble.
+
+Per desgràcia, en el sorteig d’avui ha sortit el nom de la princesa, i serà ella qui serà sacrificada per saciar al drac.
+
+Troba al drac 🐲 per continuar la història.
+'''
+
+princesa_2='''princesa2'''
+
+welcome_rei='''En aquest conte t'ha tocat ser el rei 🤴! . Ets el governant del poble, que està amenaçat per un drac ferotge. Per tal de calmar a la bèstia li doneu part dels ramats, és per això que has de parlar amb el pagès i dir-li que avui li toca sacrificar una vaca. 
+
+Troba al pagès  👨‍🌾 per poder continuar la història. ”
+'''
+
+rei_2='''rei2'''
+
+welcome_pages='''T'ha tocat ser un pagès 👨‍🌾 ! El teu dia a dia es fa bastant complicat per culpa el drac, que es menja el teu bestiar. Per tal de saciar la gana del drac, estàs participant en una iniciativa que consisteix en entregar cada dia una bestia al drac. Es per això que li has de comunicar a la teva estimada vaca que avui s’haurà de sacrificar per salvar a la resta del bestiar. 
+
+Troba la vaca 🐮 per poder continuar la història.
+'''
+
+pages_1='''Molt bé! Has trobat la vaca. Per tu no ha sigut gens fàcil entregar-la al drac, però com a mínim no molestarà
+'''
+
+pages_2='''pages2'''
+
+
+welcome_drac='''En aquest conte t’ha tocat ser el drac 🐲! . Sempre hi ha d’haver un dolent, i aquest cop t’ha tocat a tu. 
+
+El teu dia a dia és bastant tranquil, et passeges tranquil·lament per la muntanya devorant els ramats que trobes. Per algun motiu, avui et ve molt de gust devorar una vaca i no pararàs fins a trobar-ne una. 
+
+Troba una vaca 🐮 per poder continuar la història.
+'''
+
+drac_1='''Molt bé! Has trobat una vaca 🐮. Deliciosa per cert.
+
+Ara, com cada dia et dirigeixes a davant del palau, a buscar la ofrena diària, que tocarà avui?
+
+Ostres! Es comenta que avui hi ha una tendre princesa. Troba la princesa per continuar 👸🏼 .
+'''
+
+drac_2='''drac2'''
+
+welcome_vaca='''T'ha tocat ser una vaca 🐮.  És un personatge de vital importància en aquest conte.
+
+La teva vida com a vaca és bastant complicada amb un monstre assassí rondant les muntanyes, i encara més des de que t’han escollit com a ofrena pel drac. Troba al drac 🐲 per poder continuar la història"
+'''
+
+vaca_1='''Molt bé! Has trobat al drac. No has durat no 3 segons, se’t ha empassat d’una sola peça.
+'''
+
+vaca_2='''vaca2'''
+
+how_to='''*Com afegir un personatge per continuar la història?*
+
+Has d’escriure el número de telèfon de la persona que té el personatge assignat. Per exemple (34669214506). També pots 📎 *Adjuntar* el contacte.
+'''
+#   👑 rei
+#   👦🏻 primcep
 def welcome(state):
-    if state == "princep": return welcome_princep
+    if state == "cavaller": return welcome_cavaller
     elif state == "princesa": return welcome_princesa
     elif state == "rei": return welcome_rei
     elif state == "pages": return welcome_pages
     elif state == "drac": return welcome_drac
-    elif state == "bou": return welcome_bou
+    elif state == "vaca": return welcome_vaca
+
+
+
 
